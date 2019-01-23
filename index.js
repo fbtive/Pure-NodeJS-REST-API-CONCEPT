389d3302605b7d8007432b17ddc0593385f24b28
Felix Bello <bello2redfield@gmail.com>
2019-01-23 22:37:53 +0700

    add routing and request handler

--- index.js
+++ index.js
@@ -23,15 +23,36 @@ var server = http.createServer(function (req, res) {
     var decoder = new StringDecoder('utf-8');
     var buffer = '';
 
+    //assign request buffer on request listener received data
     req.on('data', function(bufferData) {
         buffer += decoder.write(bufferData);
     });
 
+    //assign handler page from router
     req.on('end', function() {
         buffer += decoder.end();
 
-        res.end("Hello World\n");
-        console.log("Buffer data received: " + buffer);
+        var chosenHandler = typeof(router[trimmedPath]) !== "undefined" 
+            ? router[trimmedPath] : handlers.notFound;
+
+        var data = {
+            trimmedPath: trimmedPath,
+            headers: requestHeaders,
+            method: requestMethod,
+            queryStringObject : queryStringObject,
+            payload: buffer
+        }
+
+        chosenHandler(data, function(statusCode, payload) {
+            statusCode = typeof(statusCode) == "number" ? statusCode : 200;
+            payload = typeof(payload) == "object" ? payload : {};
+
+            var payloadString = JSON.stringify(payload);
+
+            res.writeHead(statusCode);
+            res.end(payloadString);
+            console.log("Response data received: " + statusCode, payloadString);
+        })
     })
 
 });
@@ -39,3 +60,18 @@ var server = http.createServer(function (req, res) {
 server.listen(3000, function(){
     console.log("the server is listening on port 3000 now")
 })
+
+var handlers = {};
+
+handlers.sample = function(data, callback) {
+
+    callback(406, {'name': 'sample handler'});
+}
+
+handlers.notFound = function(data, callback) {
+    callback(404, {'name': 'Page Not Found'});
+}
+
+var router = {
+    'sample': handlers.sample
+}
